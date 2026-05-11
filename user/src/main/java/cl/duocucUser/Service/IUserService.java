4e88d96a49fc4e6c3430ea73b67836202890e81b
Alonso Randolph <al.randolph@duocuc.cl>
2026-05-11 19:06:47 -0400

    definicion de cuales van a ser los metodos de la clase

--- user/src/main/java/cl/duocucUser/Service/IUserService.java
+++ user/src/main/java/cl/duocucUser/Service/IUserService.java
@@ -1,13 +1,17 @@
 package cl.duocucUser.Service;
 
-import cl.duocucUser.Model.User;
-
+import cl.duocucUser.DTO.UserDto;
 
 import java.util.List;
 import java.util.Optional;
 
 
 public interface IUserService {
-    List<User> findAll();
-    Optional<User> findByRut(String rut);
+    List<UserDto> findAll();
+    Optional<UserDto> findByRut(String rut);
+    Optional<UserDto> findByEmail(String email);
+    UserDto save(UserDto userDto);
+    Optional<UserDto> updateFullName(String rut, String newFullName);
+    Optional<UserDto> updateEmail(String rut, String newEmail);
+    void deleteByRut(String rut);
 }
