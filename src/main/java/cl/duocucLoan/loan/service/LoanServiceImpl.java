a804d2b6885ef5fe5927e50ba2cc60955e3f538a
Alonso Randolph <al.randolph@duocuc.cl>
2026-05-14 14:12:24 -0400

    va tomando forma

--- src/main/java/cl/duocucLoan/loan/service/LoanServiceImpl.java
+++ src/main/java/cl/duocucLoan/loan/service/LoanServiceImpl.java
@@ -2,11 +2,17 @@ package cl.duocucLoan.loan.service;
 
 import cl.duocucLoan.loan.client.BookClient;
 import cl.duocucLoan.loan.client.UserClient;
+import cl.duocucLoan.loan.dto.BookResponseDto;
+import cl.duocucLoan.loan.dto.LoanRequestDto;
 import cl.duocucLoan.loan.dto.LoanResponseDto;
+import cl.duocucLoan.loan.dto.UserResponseDto;
 import cl.duocucLoan.loan.model.Loan;
 import cl.duocucLoan.loan.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,16 +22,46 @@ public class LoanServiceImpl implements ILoanService {
     private final UserClient userClient;
     private final BookClient bookClient;
 
+
     private LoanResponseDto toDto(Loan loan) {
+        UserResponseDto userDto = userClient.getUserByRut(loan.getUserRut());
+        BookResponseDto bookDto = bookClient.getBookByIsbn(loan.getBookIsbn());
         return new LoanResponseDto(
                 loan.getIdLoan(),
-                bookClient.getBookByIsbn(loan.getBookIsbn()),
-                userClient.getUserByRut(loan.getUserRut()),
+                bookDto,
+                userDto,
                 loan.getLoanDate(),
                 loan.getDueDate(),
                 loan.getReturnedDate()
         );
-
     }
+
+    private Loan toEntity(LoanRequestDto request) {
+        Loan loan = new Loan();
+        loan.setUserRut(request.getUserRut());
+        loan.setBookIsbn(request.getBookIsbn());
+        loan.setLoanDate(LocalDate.now());
+        loan.setDueDate(LocalDate.now().plusDays(14));
+        loan.setReturnedDate(null); // se usa null pq no se sabe cuando lo van a devolver
+        return loan;
+    }
+
+    @Override
+    public List<LoanResponseDto> findAll() {
+        return loanRepository.findAll()
+                .stream()
+                .map(this::toDto).toList();
+    }
+
+    @Override
+    public LoanResponseDto findById(Long idLoan) {
+        Loan loan = loanRepository.findById(idLoan).orElse(null);
+        if  (loan == null) {
+            throw new RuntimeException("prestamo de " +  idLoan + " no encontrado");
+        }
+        return toDto(loan);
+    }
+
+
 
 }
