466a789e852fa63bf3380686b399c45015da8896
Alonso Randolph <al.randolph@duocuc.cl>
2026-05-15 14:23:09 -0400

    se le asigaron a los dto de el nombre correcto que devuelve el json, para no modificar internamente nada.

--- src/main/java/cl/duocucLoan/loan/dto/BookResponseDto.java
+++ src/main/java/cl/duocucLoan/loan/dto/BookResponseDto.java
@@ -1,5 +1,6 @@
 package cl.duocucLoan.loan.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BookResponseDto {
 
+    @JsonProperty("isbn")
     private Long Bookisbn;
 
+    @JsonProperty("title")
     private String Booktitle;
 
+    @JsonProperty("author")
     private String Bookauthor;
 
 }
--- src/main/java/cl/duocucLoan/loan/dto/UserResponseDto.java
+++ src/main/java/cl/duocucLoan/loan/dto/UserResponseDto.java
@@ -1,6 +1,7 @@
 package cl.duocucLoan.loan.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +12,16 @@ import java.time.LocalDate;
 @Data @NoArgsConstructor @AllArgsConstructor
 public class UserResponseDto {
 
+    @JsonProperty("rut")
     private String Userut;
 
+    @JsonProperty("fullName")
     private String userName;
 
+    @JsonProperty("email")
     private String UserEmail;
 
+    @JsonProperty("membershipedDate")
     private LocalDate UserMembershipedDate;
 
 }
