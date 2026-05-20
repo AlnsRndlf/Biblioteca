a1f522aa56ca8fc01c7104f9c63fbeaebda1fa65
Alonso Randolph <al.randolph@duocuc.cl>
2026-05-20 11:04:07 -0400

    punto rompido

---
+++ src/main/java/cl/duocuc/penalty/IPenaltyRepository.java
@@ -0,0 +1,14 @@
+package cl.duocuc.penalty;
+
+
+import cl.duocuc.penalty.model.Penalty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IPenaltyRepository extends JpaRepository<Penalty, Long> {
+    List<Penalty> findByUserRut(String userRut);
+}
--- src/main/java/cl/duocuc/penalty/PenaltyApplication.java
+++ src/main/java/cl/duocuc/penalty/PenaltyApplication.java
@@ -2,8 +2,10 @@ package cl.duocuc.penalty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PenaltyApplication {
 
 	public static void main(String[] args) {
