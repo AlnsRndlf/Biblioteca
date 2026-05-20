1b646c39fa8c20e5634e7f06d3732e2e4f14b545
Alonso Randolph <al.randolph@duocuc.cl>
2026-05-20 14:56:06 -0400

    detalles

--- src/main/java/cl/duocuc/penalty/repository/IPenaltyRepository.java
+++ src/main/java/cl/duocuc/penalty/repository/IPenaltyRepository.java
@@ -1,9 +1,8 @@
-package cl.duocuc.penalty;
+package cl.duocuc.penalty.repository;
 
 
 import cl.duocuc.penalty.model.Penalty;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
