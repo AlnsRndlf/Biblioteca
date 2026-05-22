862222a5a008cc668d4f686da6d251f305deefa4
Alonso Randolph <al.randolph@duocuc.cl>
2026-05-22 15:05:31 -0400

    ya perdon o

--- src/main/java/cl/duocuc/review/repository/IReviewRepository.java
+++ src/main/java/cl/duocuc/review/repository/IReviewRepository.java
@@ -1,6 +1,7 @@
 package cl.duocuc.review.repository;
 
 import cl.duocuc.review.client.LoanClient;
+import cl.duocuc.review.dto.ReviewResponseDto;
 import cl.duocuc.review.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,6 @@ import java.util.List;
 @Repository
 public interface IReviewRepository extends JpaRepository<Review, Long> {
 
-    List<Review> findByBookIsbn(String bookIsbn);
+    List<Review> findByBookIsbn(Long bookIsbn);
     List<LoanClient> findByUserRut(String rut);
 }
